2dd95ba853317a047de219a4ddd4e4a725dc4691
vinay manyam <vinayip541@gmail.com>
2017-07-23 09:55:11 +0530

    Update AllProductsActivity.java

--- findme-android-app/src/ml/projects2017/AllProductsActivity.java
+++ findme-android-app/src/ml/projects2017/AllProductsActivity.java
@@ -1,1 +1,166 @@
+package ml.projects2017;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class AllProductsActivity extends ListActivity {
+
+	// Progress Dialog
+	private ProgressDialog pDialog;
+
+	// Creating JSON Parser object
+	JSONParser jParser = new JSONParser();
+
+	ArrayList<HashMap<String, String>> productsList;
+
+	// url to get all products list
+	private static String url_all_products = "url";
+
+	// JSON Node names
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_PRODUCTS = "products";
+	private static final String TAG_PID = "pid";
+	private static final String TAG_NAME = "name";
+
+	// products JSONArray
+	JSONArray products = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.all_products);
+
+		// Hashmap for ListView
+		productsList = new ArrayList<HashMap<String, String>>();
+
+		// Loading products in Background Thread
+		new LoadAllProducts().execute();
+
+		// Get listview
+		ListView lv = getListView();
+
+		// on seleting single product
+		// launching Edit Product Screen
+
+
+	}
+
+
+
+	/**
+	 * Background Async Task to Load all product by making HTTP Request
+	 * */
+	class LoadAllProducts extends AsyncTask<String, String, String> {
+
+		/**
+		 * Before starting background thread Show Progress Dialog
+		 * */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(AllProductsActivity.this);
+			pDialog.setMessage("checking connection. Please wait...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+
+		/**
+		 * getting All products from url
+		 * */
+		protected String doInBackground(String... args) {
+			// Building Parameters
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			// getting JSON string from URL
+			JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+			
+			// Check your log cat for JSON reponse
+			Log.d("All Products: ", json.toString());
+
+			try {
+				// Checking for SUCCESS TAG
+				int success = json.getInt(TAG_SUCCESS);
+
+				if (success == 1) {
+					finish();
+					// products found
+					// Getting Array of Products
+					products = json.getJSONArray(TAG_PRODUCTS);
+
+					// looping through All Products
+					for (int i = 0; i < products.length(); i++) {
+						JSONObject c = products.getJSONObject(i);
+
+						// Storing each json item in variable
+						String id = c.getString(TAG_PID);
+						String name = c.getString(TAG_NAME);
+
+						// creating new HashMap
+						HashMap<String, String> map = new HashMap<String, String>();
+
+						// adding each child node to HashMap key => value
+						map.put(TAG_PID, id);
+						map.put(TAG_NAME, name);
+
+						// adding HashList to ArrayList
+						productsList.add(map);
+					}
+				} else {
+					finish();
+					
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		protected void onPostExecute(String file_url) {
+			// dismiss the dialog after getting all products
+			pDialog.dismiss();
+			// updating UI from Background Thread
+			runOnUiThread(new Runnable() {
+				public void run() {
+					/**
+					 * Updating parsed JSON data into ListView
+					 * */
+					ListAdapter adapter = new SimpleAdapter(
+							AllProductsActivity.this, productsList,
+							R.layout.list_item, new String[] { TAG_PID,
+									TAG_NAME},
+							new int[] { R.id.pid, R.id.name });
+					// updating listview
+					setListAdapter(adapter);
+				
+				}
+		
+			});
+
+		}
+
+	}
+}
